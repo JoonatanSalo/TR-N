c72cea9e680e0a79e4b706c46e296a4f8f7022a4
JoonatanSalo <joonatan.r.salo.com>
2020-01-10 16:18:09 +0200

    Initial commit

---
+++ Assets/Enemy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int maxHealth = 2;
+    int currentHealth;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+    void TakeDamage(int damage) {
+        currentHealth -= damage;
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die() {
+        Debug.Log("Died");
+    }
+}
---
+++ Assets/PlayerMove.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public CharacterController2D controller;
+
+    public float runSpeed = 40f;
+
+    float horizontalMove = 0f;
+    bool jump = false;
+    bool crouch = false;
+    
+    void Update ()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if (Input.GetButtonDown("Jump")) 
+        {
+            jump = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftControl)) {
+            crouch = true;
+        } else if (Input.GetKeyUp(KeyCode.LeftControl)) {
+            crouch = false;
+        }
+    }
+
+    void FixedUpdate ()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
+    }
+}
