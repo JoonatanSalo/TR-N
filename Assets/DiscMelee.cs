f9ae3d584d0cfdd5ef35865e3f2efe49e22a7892
JoonatanSalo <joonatan.r.salo.com>
2020-01-11 20:59:32 +0200

    idle and melee animations

---
+++ Assets/DiscMelee.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiscMelee : MonoBehaviour
+{
+    public Animator animator;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse1)) {
+            Melee();
+        }
+    }
+
+    void Melee() 
+    {
+        animator.SetTrigger("Melee");
+    }
+}
