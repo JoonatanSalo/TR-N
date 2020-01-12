f2f55c0e7e2ea7e391d3a5b8deb6c37d4ee195f8
JoonatanSalo <joonatan.r.salo.com>
2020-01-12 17:11:02 +0200

    working melee

--- Assets/DiscMelee.cs
+++ Assets/DiscMelee.cs
@@ -6,21 +6,48 @@ public class DiscMelee : MonoBehaviour
 {
     public Animator animator;
 
+    public Transform meleePoint;
+    public float attackRange = 0.5f;
+    public LayerMask destrucktableLayers;
+    public int meleeDamage = 1;
+    public float attackRate = 2f;
+    float nextAttackTime = 0f;
+
     void Start()
     {
         
     }
 
-    
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Mouse1)) {
-            Melee();
+        if(Time.time >= nextAttackTime)
+        {
+            if (Input.GetKeyDown(KeyCode.Mouse1))
+            {
+                Melee();
+
+                nextAttackTime = Time.time + 1f / attackRate;
+            }
         }
     }
 
     void Melee() 
     {
         animator.SetTrigger("Melee");
+
+        Collider2D[] hitObjects = Physics2D.OverlapCircleAll(meleePoint.position, attackRange, destrucktableLayers);
+
+        foreach (Collider2D enemy in hitObjects)
+        {
+            enemy.GetComponent<Enemy>().TakeDamage(meleeDamage);
+        }
+;   }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (meleePoint == null)
+            return;
+
+        Gizmos.DrawWireSphere(meleePoint.position, attackRange);
     }
 }
